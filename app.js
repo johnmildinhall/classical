c4eb8e9bc2f9ebf17f530e1c651a0d1f21be2eb8
johnmildinhall <johnmildinhall@Megatron.local>
2015-04-22 14:43:31 +0100

    flexible port number

--- app.js
+++ app.js
@@ -141,5 +141,6 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-console.log('Listening on 8888');
-app.listen(8888);
+var port = Number(process.env.PORT || 3000)
+console.log('Listening on '+port);
+app.listen(port);
